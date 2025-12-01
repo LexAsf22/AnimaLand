1a9ebfb0144a7f973f4dc2542432f04c53c670e6
렉서스 <vanphillip.tamayo@lorma.edu>
2025-12-01 16:36:49 +0800

    pahabol po

---
+++ my-react-app/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-white shadow px-6 py-4 text-center text-gray-600 text-sm">
+      &copy; {new Date().getFullYear()} AnimaLand Admin Dashboard. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
